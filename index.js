bd0390597b6e147edc5f3dfcf59feb44ab2ba01f
Daryna Riabova <darinar474@gmail.com>
2024-01-05 16:10:03 +0100

    fix form

--- index.js
+++ index.js
@@ -2,30 +2,44 @@ const form = document.getElementById("form");
 const username = document.getElementById("name");
 const email = document.getElementById("email");
 const message = document.getElementById("message");
+
 form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  checkInputs();
+  if (!checkInputs()) {
+    e.preventDefault();
+  }
 });
+
 function checkInputs() {
+  let isValid = true;
+
   const usernameValue = username.value.trim();
   const emailValue = email.value.trim();
   const messageValue = message.value;
+
   if (usernameValue === "") {
     setErrorFor(username);
+    isValid = false;
   } else {
     setSuccessFor(username);
   }
+
   if (emailValue === "") {
     setErrorFor(email);
+    isValid = false;
   } else {
     setSuccessFor(email);
   }
+
   if (messageValue === "") {
     setErrorFor(message);
+    isValid = false;
   } else {
     setSuccessFor(message);
   }
+
+  return isValid;
 }
+
 function setErrorFor(input) {
   const formControl = input.parentElement;
   formControl.className = "newsletter__form-control error";
